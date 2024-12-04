8e78b4222a0657847e2437164885c08bc963cf89
muslmoney <ergashevbehruz.uz@gmail.com>
2024-12-04 19:16:49 +0500

    Half part

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_cubit/bloc/home_cubit.dart';
 import 'package:bloc_cubit/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +19,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: BlocProvider(
+        create: (BuildContext context) => HomeCubit(),
+        child: HomePage(),
+      ),
     );
   }
 }
