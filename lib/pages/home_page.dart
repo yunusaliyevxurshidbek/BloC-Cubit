4e6ccf8250a80bc4d81f5687e808d18fdbec2945
Xurshidbek <xurshidbek4466@gmail.com>
2024-12-05 11:39:29 +0500

    Final comment

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:bloc_cubit/bloc/home_cubit.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:bloc_cubit/views/item_of_random_user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/home_state.dart';
 import '../models/random_user_list_res.dart';
-import '../services/http_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,99 +16,71 @@ class _HomePageState extends State<HomePage> {
   late HomeCubit homeCubit;
   ScrollController scrollController = ScrollController();
 
-
   @override
   void initState() {
     super.initState();
     homeCubit = BlocProvider.of<HomeCubit>(context);
     homeCubit.onLoadRandomUserList();
 
-
     scrollController.addListener(() {
-      if (scrollController.position.maxScrollExtent <= scrollController.offset) {
+      if (scrollController.position.maxScrollExtent <=
+          scrollController.offset) {
         homeCubit.onLoadRandomUserList();
       }
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromRGBO(232, 232, 232, 1),
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text("Random User - SetState"),
-        ),
-        body: Stack(
-          children: [
-            ListView.builder(
-              controller: scrollController,
-              itemCount: userList.length,
-              itemBuilder: (ctx, index) {
-                return _itemOfRandomUser(userList[index], index);
-              },
-            ),
-            isLoading
-                ? const Center(
-              child: CircularProgressIndicator(),
-            )
-                : const SizedBox.shrink(),
-          ],
-        ));
+      backgroundColor: const Color.fromRGBO(232, 232, 232, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("Random User - SetState"),
+      ),
+      body: BlocBuilder<HomeCubit, HomeState>(
+
+
+        //! Scroll paytida agar buildWhen ishlatilmasa yana boshiga qaytib qoladi backendan data olgandan so'ng
+        buildWhen: (previous,current){
+          return current is HomeRandomUserListState;
+        },
+
+
+        builder: (BuildContext context, HomeState state) {
+          if (state is HomeErrorState) {
+            return viewOfError(state.errorMessage);
+          }
+          if (state is HomeRandomUserListState) {
+            var userList = state.userList;
+            return viewOfRandomUserList(userList);
+          }
+
+          return viewOfLoading();
+        },
+      ),
+    );
   }
 
-  Widget _itemOfRandomUser(RandomUser randomUser, int index) {
-    return Container(
-        color: Colors.white,
-        margin: const EdgeInsets.only(top: 5),
-        padding: const EdgeInsets.all(16),
-        child: Row(
-          children: [
-            CachedNetworkImage(
-              height: 80,
-              width: 80,
-              fit: BoxFit.cover,
-              imageUrl: randomUser.picture.medium,
-              placeholder: (context, url) => Container(
-                height: 80,
-                width: 80,
-                color: Colors.grey,
-              ),
-              errorWidget: (context, url, error) => Container(
-                height: 80,
-                width: 80,
-                color: Colors.grey,
-                child: const Icon(Icons.error),
-              ),
-            ),
-            const SizedBox(
-              width: 16,
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "${index} - ${randomUser.name.first} ${randomUser.name.last}",
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  Text(
-                    randomUser.email,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.normal, fontSize: 16),
-                  ),
-                  Text(
-                    randomUser.cell,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.normal, fontSize: 16),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ));
+  Widget viewOfError(String err) {
+    return Center(
+      child: Text("Could not fetch your data"),
+    );
+  }
+
+  Widget viewOfLoading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget viewOfRandomUserList(List<RandomUser> userList) {
+    return ListView.builder(
+      controller: scrollController,
+      itemCount: userList.length,
+      itemBuilder: (context, index) {
+        return itemOfRandomUser(userList[index], index);
+      },
+    );
   }
 }
